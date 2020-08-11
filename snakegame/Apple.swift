2ca68a17d84422f2475e9cfb319e670d332a3c71
MaximTimokhin <ffoka96@icloud.com>
2020-08-11 16:05:12 +0300

    8HW

---
+++ snakegame/Apple.swift
@@ -0,0 +1,9 @@
+//
+//  Apple.swift
+//  snakegame
+//
+//  Created by Максим Тимохин on 11.08.2020.
+//  Copyright © 2020 Максим Тимохин. All rights reserved.
+//
+
+import Foundation
---
+++ snakegame/SnakeBodyParth.swift
@@ -0,0 +1,9 @@
+//
+//  SnakeBodyParth.swift
+//  snakegame
+//
+//  Created by Максим Тимохин on 11.08.2020.
+//  Copyright © 2020 Максим Тимохин. All rights reserved.
+//
+
+import Foundation
